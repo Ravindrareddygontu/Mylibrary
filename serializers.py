c7d3459ffa136250cb2209155b1d2166a17196ad
Ravindrareddygontu <102969952+Ravindrareddygontu@users.noreply.github.com>
2022-04-05 17:43:05 +0530

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -5,18 +5,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'from rest_framework import serializers
-from .models import Book
-
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'from rest_framework import serializers
-from .models import Book
-
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
         fields = '__all__'
